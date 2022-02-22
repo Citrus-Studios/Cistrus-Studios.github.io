714fb1cf2ec94ff0b8a0641e3cff23e7bd036de8
lonnonjamesd <lemonjameslem@protonmail.com>
2022-02-21 19:08:32 -0500

    Added More than one page support

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
+import ReactDOM from "react-dom";
+import Home from './Home/Home';
+import Blogs from './Blogs/Blog';
 import './index.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    Outlet,
+} from "react-router-dom";
+
+export default function App() {
+    return(
+        <div>
+            <nav>
+                <ul>
+                <Link to="/">Home</Link>
+                </ul><ul>
+                <Link to="/blogs">Blogs</Link>
+                </ul>
+            </nav>
+            <Outlet/>
+        </div>
+    );
+}
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+    <Router>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route path="/" element={<Home />} />
+                <Route path="blogs" element={<Blogs />} />
+            </Route>
+        </Routes>
+    </Router>,
     document.getElementById('root')
 )
 
