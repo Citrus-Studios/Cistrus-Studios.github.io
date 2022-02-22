51abffa2075d5f831a573229a0f75b69b566e249
lonnonjamesd <lemonjameslem@protonmail.com>
2022-02-21 19:08:15 -0500

    Renamed App to Home

---
+++ src/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="grid place-items-center h-screen">
+                <h1>Welcome</h1>
+            </div>
+        )
+    }
+}
