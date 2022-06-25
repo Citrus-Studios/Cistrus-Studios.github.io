4d83a38ecdba4ae9d42d1a9fc0ef7fda086e40d3
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-25 18:03:08 -0400

    Removed Some code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
-}
+enum Msg {}
 
 struct Model {
     value: i64,
@@ -17,25 +15,11 @@ impl Component for Model {
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
+        false
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
+        html! {}
     }
 }
 
